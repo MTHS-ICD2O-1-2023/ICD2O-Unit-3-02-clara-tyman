554def890c37cb7f58b5fa6ae302acd210c6800e
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-04-03 18:37:38 +0000

    html js and css story and flow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara Tyman
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+/**
+ * This function calculates 
+ */
+function calculate() {
+  // input
+  const length = parseFloat(document.getElementById('length-of-pyramid').value)
+  const width = parseFloat(document.getElementById('width-of-pyramid').value)
+  const height = parseFloat(document.getElementById('height-of-pyramid').value)
+
+  // process
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+}
